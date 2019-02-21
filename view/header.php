1bed5b98b680d17b1617e8303b2c66b017c07e5c
LuigiArreaga <lui.amarillo@hotmail.com>
2019-02-21 10:32:43 -0500

    Update header.php

--- view/header.php
+++ view/header.php
@@ -8,6 +8,14 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Optica MG</title>
+    
+        <link rel="stylesheet" href="assets/css/Footer-with-button-logo.css">
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+        
+        <link rel="stylesheet" href="css/efecto_imagen.css">
+
+
+
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
