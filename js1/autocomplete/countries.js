e8c86d9fba89dae7430dcbd59be8941fd54ac3b9
LuigiArreaga <lui.amarillo@hotmail.com>
2019-02-21 00:22:52 -0500

    js

---
+++ js1/autocomplete/countries.js
@@ -0,0 +1,16 @@
+﻿var countries = {
+   
+    "AM": "Ambato", 
+    "CU": "Cuenca", 
+    "EL": "El Empalme",
+    "LO": "Loja",
+    "GU": "Guayaquil",
+    "QU": "Quevedo",
+    "NA": "Naranjal",
+    "BY": "Babahoyo",
+    "DU": "Duran",
+    "ES": "Esmeraldas",
+    "BA": "Balzar",
+    "NO": "Nobol",
+    
+}
