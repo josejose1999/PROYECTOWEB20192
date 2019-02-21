ed04dbeb64c0447ca4d27917e3726d26b076d5d2
LuigiArreaga <lui.amarillo@hotmail.com>
2019-02-21 13:22:08 -0500

    controlador

--- index.php
+++ index.php
@@ -47,25 +47,28 @@ if(!isset($_REQUEST['c']))
     $controller = new $controller;
     $controller->IndexAdministrar();  
     }else if(($_REQUEST['c'])=='Usuario')
+
+if(!isset($_REQUEST['c']))
 {
+
     require_once "controller/$controller2.controller.php";
     $controller2 = ucwords($controller2) . 'Controller';
     $controller2 = new $controller2;
     $controller2->Index();    
 }
-// else
-// {
-//     // Obtenemos el controlador que queremos cargar
-//     $controller2 = strtolower($_REQUEST['c']);
-//     $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
+
+else{
+    // Obtenemos el controlador que queremos cargar
+    $controller2 = strtolower($_REQUEST['c']);
+    $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
     
-//     // Instanciamos el controlador
-//     require_once "controller/$controller2.controller.php";
-//     $controller2 = ucwords($controller2) . 'Controller';
-//     $controller2 = new $controller2;
+    // Instanciamos el controlador
+    require_once "controller/$controller2.controller.php";
+    $controller2 = ucwords($controller2) . 'Controller';
+    $controller2 = new $controller2;
     
-//     // Llama la accion
-//     call_user_func( array( $controller2, $accion ) );
-// }
+    // Llama la accion
+    call_user_func( array( $controller2, $accion ) );
+}
 }
 
