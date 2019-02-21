6c1d7c4ed5de68253842222d773623b4f2456120
LuigiArreaga <lui.amarillo@hotmail.com>
2019-02-20 23:03:07 -0500

    BUFFON BORRO TODO

--- conn.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbhost	= "localhost";	   // localhost or IP
-$dbuser	= "root";		  // database username
-$dbpass	= "";		     // database password
-$dbname	= "PHPLogin";    // database name
-?>
