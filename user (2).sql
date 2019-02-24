83b2e37463f41b3b9b84100f9617cb22b00b9a42
sebasqwer1 <47837778+sebasqwer1@users.noreply.github.com>
2019-02-23 18:48:19 -1200

    sebas
    base de datos act

--- user (2).sql
+++ user (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-02-2019 a las 03:28:18
+-- Tiempo de generación: 24-02-2019 a las 07:47:17
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.3.0
 
@@ -30,20 +30,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `productos` (
   `id` int(11) NOT NULL,
-  `Nombre` varchar(50) NOT NULL DEFAULT '0',
-  `Imagen` varchar(200) NOT NULL DEFAULT '0',
-  `Precio` float NOT NULL DEFAULT '0'
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `image` varchar(200) NOT NULL DEFAULT '0',
+  `price` double(10,2) NOT NULL DEFAULT '0.00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `Nombre`, `Imagen`, `Precio`) VALUES
-(14, 'sas', 'sasa', 1221),
-(16, 'asas', 'sasa', 23345),
-(17, 'assas', 'dsdss', 5454),
-(19, 'saasa', 'assassas', 123435000000);
+INSERT INTO `productos` (`id`, `name`, `image`, `price`) VALUES
+(14, 'lente acuatico', '1.jpg', 1221.00),
+(17, 'lente de sol', '3.jpg', 5454.00),
+(19, 'lente de viejo', '15.jpg', 99999999.99),
+(20, 'lente sapo', '6.jpg', 123.00),
+(21, 'lente de zorro', '7.jpg', 989.00),
+(22, 'lente cafe', '2.jpg', 600.00);
 
 -- --------------------------------------------------------
 
@@ -92,7 +94,7 @@ INSERT INTO `usuarios` (`id`, `Nombre`, `Apellido`, `Sexo`, `FechaNacimiento`, `
 (4, 'sebastian', 'adsadd', 1, '3183-02-12', '2019-02-21', 'sdsjd@gof.com', 'sebas', '0d40d9aea2b3b5a149dc34495fff0a0a', 2, ''),
 (5, 'Liset Sapa', 'Cabeza de Pato', 2, '3932-03-21', '2019-02-21', 'dsdj@hof.com', 'lise', 'e8d2a7b40868c9953638abcdae19e4ed', 1, ''),
 (7, 'sjasasj', 'djadsjs', 1, '2283-03-12', '2019-02-21', 'dsds@gofdk.com', '', '', 0, ''),
-(8, 'cabeza de sapo', 'qekesjd', 1, '2332-03-12', '2019-02-21', '€wsdd@gjdjs.com', '', '', 0, ''),
+(8, 'sapo', 'qekesjd', 1, '2332-03-12', '2019-02-21', 'wsdd@gmail.com', 'sapo', '28ea19352381b8659df830dd6d5c90a3', 1, ''),
 (9, 'Luigi', 'Arreaga', 0, '1998-02-06', NULL, 'luigi.arreagawda@djxj.com', 'luigi', 'd9b1d7db4cd6e70935368a1efb10e377', 1, '2019-02-23');
 
 --
@@ -125,7 +127,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
