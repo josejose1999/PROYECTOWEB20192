f593e6c1a07b2ed8999220166035b2a91bbbf45c
mariajosesc9 <47908440+mariajosesc9@users.noreply.github.com>
2019-02-24 13:44:40 -0500

    Base de datos
    La base de datos ahora se llama optica por lo tanto actualicenla y ya está relacionada con las claves foráneas. Creen de nuevo sus bases de datos que ya probé y ya está funcionando todo.

--- user_actualizada.sql
+++ user_actualizada.sql
@@ -19,8 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `user`
+-- Base de datos: `optica`
 --
+CREATE DATABASE IF NOT EXISTS `optica` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+USE `optica`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +30,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `citas`
 --
 
-CREATE TABLE `citas` (
+CREATE TABLE IF NOT EXISTS `citas` (
   `id` int(11) NOT NULL,
   `FechaTentativa` date NOT NULL,
   `HoraCita` time(6) NOT NULL,
@@ -36,7 +38,8 @@ CREATE TABLE `citas` (
   `Nombre` varchar(100) NOT NULL,
   `Celular` varchar(100) NOT NULL,
   `Telefono` varchar(100) NOT NULL,
-  `Correo` varchar(50) NOT NULL
+  `Correo` varchar(50) NOT NULL,
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -54,12 +57,12 @@ INSERT INTO `citas` (`id`, `FechaTentativa`, `HoraCita`, `NombreOptica`, `Nombre
 --
 -- Estructura de tabla para la tabla `productos`
 --
-
-CREATE TABLE `productos` (
+CREATE TABLE IF NOT EXISTS `productos` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL DEFAULT '0',
   `image` varchar(200) NOT NULL DEFAULT '0',
-  `price` double(10,2) NOT NULL DEFAULT '0.00'
+  `price` double(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -80,9 +83,10 @@ INSERT INTO `productos` (`id`, `name`, `image`, `price`) VALUES
 -- Estructura de tabla para la tabla `tipo_usuario`
 --
 
-CREATE TABLE `tipo_usuario` (
+CREATE TABLE IF NOT EXISTS `tipo_usuario` (
   `id_tipo` int(11) NOT NULL,
-  `tipo` varchar(20) NOT NULL
+  `tipo` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_tipo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -99,7 +103,7 @@ INSERT INTO `tipo_usuario` (`id_tipo`, `tipo`) VALUES
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuarios` (
+CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL,
   `Nombre` varchar(50) NOT NULL DEFAULT '0',
   `Apellido` varchar(50) NOT NULL DEFAULT '0',
@@ -110,7 +114,8 @@ CREATE TABLE `usuarios` (
   `username` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
   `id_tipo` int(11) NOT NULL,
-  `foto` varchar(150) NOT NULL
+  `foto` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -120,45 +125,32 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id`, `Nombre`, `Apellido`, `Sexo`, `FechaNacimiento`, `FechaRegistro`, `Correo`, `username`, `password`, `id_tipo`, `foto`) VALUES
 (4, 'sebastian', 'adsadd', 1, '3183-02-12', '2019-02-21', 'sdsjd@gof.com', 'sebas', '0d40d9aea2b3b5a149dc34495fff0a0a', 2, ''),
 (5, 'Liset Sapa', 'Cabeza de Pato', 2, '3932-03-21', '2019-02-21', 'dsdj@hof.com', 'lise', 'e8d2a7b40868c9953638abcdae19e4ed', 1, ''),
-(7, 'sjasasj', 'djadsjs', 1, '2283-03-12', '2019-02-21', 'dsds@gofdk.com', '', '', 0, ''),
+(7, 'sjasasj', 'djadsjs', 1, '2283-03-12', '2019-02-21', 'dsds@gofdk.com', '', '', 1, ''),
 (8, 'sapo', 'qekesjd', 1, '2332-03-12', '2019-02-21', 'wsdd@gmail.com', 'sapo', '28ea19352381b8659df830dd6d5c90a3', 1, ''),
 (9, 'Luigi', 'Arreaga', 0, '1998-02-06', NULL, 'luigi.arreagawda@djxj.com', 'luigi', 'd9b1d7db4cd6e70935368a1efb10e377', 1, '2019-02-23');
 
 --
--- Índices para tablas volcadas
+-- Estructura para la tabla factura
 --
+CREATE TABLE IF NOT EXISTS `factura` (
+  `num_factura` int(12) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL,
+  `fecha` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`num_factura`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Estructura para la tabla detalle
+--
+CREATE TABLE IF NOT EXISTS `detalle` (
+  `num_detalle` varchar(10) NOT NULL,
+  `num_factura` int(12) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(5) NOT NULL,
+  `precio` double(7,2) NOT NULL,
+  PRIMARY KEY (`num_detalle`,`num_factura`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Indices de la tabla `citas`
---
-ALTER TABLE `citas`
-  ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tipo_usuario`
---
-ALTER TABLE `tipo_usuario`
-  ADD PRIMARY KEY (`id_tipo`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `citas`
---
 ALTER TABLE `citas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
@@ -174,6 +166,11 @@ ALTER TABLE `productos`
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
+
+alter table `factura` add foreign key (`id_cliente`) references `usuarios`(`id`);
+alter table `detalle` add foreign key (`id_producto`) references `productos`(`id`);
+alter table `detalle` add foreign key (`num_factura`) references `factura`(`num_factura`);
+alter table `usuarios` add foreign key (`id_tipo`) references `tipo_usuario`(`id_tipo`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
