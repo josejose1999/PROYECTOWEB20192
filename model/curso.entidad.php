d6a5129bfc80b88be5ac713dee3cd1c200ca392b
sebasqwer1 <47837778+sebasqwer1@users.noreply.github.com>
2019-02-25 03:53:09 -1200

    matado

--- model/curso.entidad.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class Curso
-    {
-        private $id;
-        private $Nombre;
-
-        public function __GET($k){ return $this->$k; }
-        public function __SET($k, $v){ return $this->$k = $v; }
-    }
