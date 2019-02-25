5bb12f152f212c6ec397bc741102d867422b8010
sebasqwer1 <47837778+sebasqwer1@users.noreply.github.com>
2019-02-24 17:26:56 -1200

    Solo se puede comprar si el usuario esta registrado

--- view/index.php
+++ view/index.php
@@ -2,38 +2,44 @@
 session_start();
 $connect = mysqli_connect("localhost", "root", "", "optica");
 
-if(isset($_POST["add_to_cart"]))
-{
-	if(isset($_SESSION["shopping_cart"]))
-	{
-		$item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
-		if(!in_array($_GET["id"], $item_array_id))
-		{
-			$count = count($_SESSION["shopping_cart"]);
-			$item_array = array(
-				'item_id'			=>	$_GET["id"],
-				'item_name'			=>	$_POST["hidden_name"],
-				'item_price'		=>	$_POST["hidden_price"],
-				'item_quantity'		=>	$_POST["quantity"]
-			);
-			$_SESSION["shopping_cart"][$count] = $item_array;
-		}
-		else
-		{
-			echo '<script>alert("Este Producto Ya Fue Añadido")</script>';
-		}
-	}
-	else
-	{
-		$item_array = array(
-			'item_id'			=>	$_GET["id"],
-			'item_name'			=>	$_POST["hidden_name"],
-			'item_price'		=>	$_POST["hidden_price"],
-			'item_quantity'		=>	$_POST["quantity"]
-		);
-		$_SESSION["shopping_cart"][0] = $item_array;
-	}
-}
+if(isset($_POST["add_to_cart"]) )
+    if (isset($_SESSION['user'])) {
+
+			{
+				if(isset($_SESSION["shopping_cart"]))
+				{
+					$item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+					if(!in_array($_GET["id"], $item_array_id))
+					{
+						$count = count($_SESSION["shopping_cart"]);
+						$item_array = array(
+							'item_id'			=>	$_GET["id"],
+							'item_name'			=>	$_POST["hidden_name"],
+							'item_price'		=>	$_POST["hidden_price"],
+							'item_quantity'		=>	$_POST["quantity"]
+						);
+						$_SESSION["shopping_cart"][$count] = $item_array;
+					}
+					else
+					{
+						echo '<script>alert("Este Producto Ya Fue Añadido")</script>';
+					}
+				}
+				else
+				{
+					$item_array = array(
+						'item_id'			=>	$_GET["id"],
+						'item_name'			=>	$_POST["hidden_name"],
+						'item_price'		=>	$_POST["hidden_price"],
+						'item_quantity'		=>	$_POST["quantity"]
+					);
+					$_SESSION["shopping_cart"][0] = $item_array;
+				}
+			}
+    }
+    else{
+  		echo '<script>alert("Registrese Para Pode Comprar")</script>';
+    }
 
 if(isset($_GET["action"]))
 {
